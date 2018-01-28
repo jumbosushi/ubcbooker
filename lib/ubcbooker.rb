26c8d7013b716e523af92db66eca3761026a8b63
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2018-01-28 13:19:09 -0800

    Add SUPPORTED_DEPARTMENTS

--- lib/ubcbooker.rb
+++ lib/ubcbooker.rb
@@ -1,24 +1,22 @@
 require "pry"
 require "mechanize"
 require "io/console" # For handling password
+require "optparse"
 require "yaml"
 
 module Ubcbooker
   # Stop at 7:14
   # TODO: before 0.1
   # - TDD
-  # - Add Optparse lib
-  #   - h
-  #   - u
   # - Catch when login fails
-  # - Feature to save password - do this so you don't have to type everytime
-  #   - How to save
-  #   - Be able to update the config with -u
-  #   take a look into it from like rails perspective
-  #   if not just save it as plain text for now
+  SUPPORTED_DEPARTMENTS = [
+    "cs",
+    "sauder",
+  ]
 end
 
 require "ubcbooker/version"
+require "ubcbooker/color"
 require "ubcbooker/config"
 require "ubcbooker/http"
 require "ubcbooker/cli"
