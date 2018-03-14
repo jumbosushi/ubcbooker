59a24f7dfa0ad14d6fb944a5d967892151234839
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2018-03-13 19:50:54 -0700

    Specs for cli.rb

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,5 +1,127 @@
 RSpec.describe Ubcbooker::CLI do
 
-  subject(:cli) { Ubcbooker::CLI.new }
+  # Disable warning msg for overwritting ARGV
+  before(:all) do
+    $VERBOSE = nil
+  end
 
+  # Enable warning msg for overwritting ARGV
+  after(:all) do
+    $VERBOSE = false
+  end
+
+  let(:cli) { Ubcbooker::CLI.new }
+
+  describe "#get_department_scraper" do
+    context "when param is cs" do
+      it "return CS Scrapper class" do
+        result = cli.get_department_scraper("cs")
+        expect(result).to eq(Ubcbooker::Scraper::Cs)
+      end
+    end
+
+    context "when param is unknown department" do
+      it "raise UnsupportedDepartment error" do
+        expect do
+          cli.get_department_scraper("boozilogy")
+        end.to raise_error(Ubcbooker::Error::UnsupportedDepartment)
+      end
+    end
+  end
+
+  describe "#parse_options" do
+    context "when ARGV is valid"
+    it "returns valid options" do
+      ARGV = ["-b", "cs",
+              "-n", "test_name",
+              "-d", "03/15",
+              "-t", "15:30-17:00"]
+      expected_options = {
+        name: "test_name",
+        date: "03/15",
+        time: "15:30-17:00",
+        department: "cs",
+      }
+      result = cli.parse_options
+      expect(result).to eq(expected_options)
+    end
+
+    context "when required options are missing" do
+      it "raise Ubcbooker::Error::MissingRequired" do
+        # Missing -n flag
+        ARGV = ["-b", "cs",
+                "-d", "03/15",
+                "-t", "15:30-17:00"]
+        expect do
+          cli.parse_options
+        end.to raise_error(Ubcbooker::Error::MissingRequired)
+      end
+    end
+
+    context "when -b is unsupported department" do
+      it "raise Ubcbooker::Error::UnsupportedDepartment" do
+        # Missing -n flag
+        ARGV = ["-b", "booziogy",
+                "-n", "test_name",
+                "-d", "03/15",
+                "-t", "15:30-17:00"]
+        expect do
+          cli.parse_options
+        end.to raise_error(Ubcbooker::Error::UnsupportedDepartment)
+      end
+    end
+
+    context "when -n contains profanity" do
+      it "raise Ubcbooker::Error::ProfaneName" do
+        # Missing -n flag
+        ARGV = ["-b", "cs",
+                "-n", "fucking group work",
+                "-d", "03/15",
+                "-t", "15:30-17:00"]
+        expect do
+          cli.parse_options
+        end.to raise_error(Ubcbooker::Error::ProfaneName)
+      end
+    end
+
+    context "when -d is unsupported date" do
+      it "raise Ubcbooker::Error::UnsupportedDate" do
+        # Missing -n flag
+        ARGV = ["-b", "cs",
+                "-n", "test_name",
+                "-d", "99/99",
+                "-t", "15:30-17:00"]
+        expect do
+          cli.parse_options
+        end.to raise_error(Ubcbooker::Error::UnsupportedDate)
+      end
+    end
+
+    context "when -u" do
+      it "call Ubcbooker::Config#ask" do
+        # Missing -n flag
+        ARGV = ["-u"]
+        cli_config = cli.instance_variable_get(:@config)
+        expect(cli_config).to receive(:ask)
+        cli.parse_options
+      end
+    end
+
+    context "when -v" do
+      it "prints version" do
+        # Missing -n flag
+        ARGV = ["-v"]
+        expect { cli.parse_options }.to output(Ubcbooker::VERSION).to_stdout
+      end
+    end
+  end
+
+  describe "#get_department_scraper" do
+    context "when param is cs" do
+      it "return CS Scrapper class" do
+        result = cli.get_department_scraper("cs")
+        expect(result).to eq(Ubcbooker::Scraper::Cs)
+      end
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,19 @@ require "bundler/setup"
 require "ubcbooker"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
-
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  # Allow exit to happen within the code
+  config.around(:example) do |ex|
+    begin
+      ex.run
+    rescue SystemExit => e
+      puts "Got SystemExit: #{e.inspect}. Ignoring"
+    end
   end
 end
